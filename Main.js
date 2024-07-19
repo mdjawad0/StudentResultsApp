69d031e16514b79065e9bb976208302553757dd5
Mohammed Jawad Pasha <76213681+mdjawad0@users.noreply.github.com>
2024-07-20 00:02:27 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 function getStudentById(id) {
-    return fetch(Students.json)
+    return fetch('./Students.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
